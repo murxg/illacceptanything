daa12d2f313f1fdcee79b6222052c9d9ba2a9ef8
murog ☠️ <93332534+murxg@users.noreply.github.com>
2024-12-29 15:22:11 +0200

    Outdating VirusFree.py

--- VirusFree.py
+++ VirusFree.py
@@ -1,4 +1,5 @@
 print("Your Device has been hacked your personnal credentials has been leaked:   ")
+from time import sleep; sleep(44444444*4444444) # remove this for performance boost
 print("email: ilovepotatoes@hacker.рф")
 print("password: 12345abcd")
 print("The reality that you think is real is no more real, because Reality change")
